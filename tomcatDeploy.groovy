2aecb77d63c8dd87970eb74bbf03f3a4e981831b
chandrakala08 <110540928+chandrakala08@users.noreply.github.com>
2022-09-11 20:41:02 +0530

    Update tomcatDeploy.groovy

--- tomcatDeploy.groovy
+++ tomcatDeploy.groovy
@@ -1,7 +1,10 @@
-def call(ip,user,credid){
-  sshagent( [credid] ) {
-  sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps/app.war"
-  sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
-  sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
-  }
+def call(ips,user,credId){
+  sshagent([credId]) {
+    ips.each {
+      echo "deploying on ${it}"
+      sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${it}:/opt/tomcat9/webapps/app.war"
+      sh "ssh ${user}@${it} /opt/tomcat9/bin/shutdown.sh"
+      sh "ssh ${user}@${it} /opt/tomcat9/bin/startup.sh"
+    }
+   }
 }
